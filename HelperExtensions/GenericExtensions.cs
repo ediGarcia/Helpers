7bb981e37dd28bc28d40e618fffe00bec5f1ac11
avaz <alex.vaz@capgemini.com>
2023-12-19 19:30:36 -0300

    Copy methods removed.

--- HelperExtensions/GenericExtensions.cs
+++ HelperExtensions/GenericExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Reflection;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace HelperExtensions;
 
@@ -13,85 +14,21 @@ public static class GenericExtensions
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="obj"></param>
-    /// <param name="deepClone">Indicates whether the inner properties and fields should have their values cloned too, instead of just the reference.</param>
-    /// <returns>A clone of the current object.</returns>
     /// <exception cref="MissingMethodException" />
     /// <remarks>The copied class type (also its properties and fields types, if deep cloning) cannot be abstract and must have a parameterless constructor, otherwise a <see cref="MissingMethodException" /> will be thrown.</remarks>
-    public static T Clone<T>(this T obj, bool deepClone = false)
+    public static T Clone<T>(this T obj)
     {
-        T newItem = Activator.CreateInstance<T>();
-        obj.CopyTo(newItem, deepClone);
-        return newItem;
-    }
-    #endregion
+        if (obj is null)
+            return default;
 
-    #region CopyFrom
-    /// <summary>
-    /// Copies the values of the specified instance to the current instance.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="obj"></param>
-    /// <param name="source"></param>
-    /// <param name="deepCopy">Indicates whether the inner properties and fields should have their values copied too, instead of just the reference.</param>
-    /// <exception cref="MissingMethodException" />
-    /// <remarks>The copied class type (also its properties and fields types, if deep copying) cannot be abstract and must have a parameterless constructor, otherwise a <see cref="MissingMethodException" /> will be thrown.</remarks>
-    public static void CopyFrom<T>(this T obj, T source, bool deepCopy = false) =>
-        CopyValues(source, obj, deepCopy);
-    #endregion
+        BinaryFormatter serializer = new();
+        using MemoryStream stream = new();
+        
+        serializer.Serialize(stream, obj);
+        stream.Flush();
+        stream.Position = 0;
 
-    #region CopyTo
-    /// <summary>
-    /// Copies the values of the current instance to the specified instance.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="obj"></param>
-    /// <param name="destination"></param>
-    /// <param name="deepCopy">Indicates whether the inner properties and fields should have their values copied too, instead of just the reference.</param>
-    /// <exception cref="MissingMethodException" />
-    /// <remarks>The copied class type (also its properties and fields types, if deep copying) cannot be abstract and must have a parameterless constructor, otherwise a <see cref="MissingMethodException" /> will be thrown.</remarks>
-    public static void CopyTo<T>(this T obj, T destination, bool deepCopy = false) =>
-        CopyValues(obj, destination, deepCopy);
-    #endregion
-
-    #endregion
-
-    #region Private Methods
-
-    #region CopyValuesPrivate
-    /// <summary>
-    /// Copies the properties' and fields' values from the source to the destination.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="source"></param>
-    /// <param name="destination"></param>
-    /// <param name="deepCopy"></param>
-    private static void CopyValues<T>(T source, T destination, bool deepCopy)
-    {
-        typeof(T).GetFields().ForEach(_ =>
-        {
-            if (_.IsStatic || _.IsInitOnly)
-                return;
-
-            object sourceFieldValue = _.GetValue(source);
-
-            if (sourceFieldValue is not null && deepCopy && _.FieldType.GetTypeInfo().IsClass)
-                CopyValues(sourceFieldValue, _.GetValue(destination), true);
-            else
-                _.SetValue(destination, sourceFieldValue);
-        });
-
-        typeof(T).GetProperties().ForEach(_ =>
-        {
-            if (!_.CanRead || !_.CanWrite)
-                return;
-
-            object sourcePropValue = _.GetValue(source);
-
-            if (sourcePropValue is not null && deepCopy && _.PropertyType.GetTypeInfo().IsClass)
-                CopyValues(sourcePropValue, _.GetValue(destination), true);
-            else
-                _.SetValue(destination, sourcePropValue);
-        });
+        return (T)serializer.Deserialize(stream);
     }
     #endregion
 
