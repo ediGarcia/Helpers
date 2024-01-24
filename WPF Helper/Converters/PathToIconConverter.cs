541b76686d4fc9ffc88797cd78ac2702be330704
avaz <alex.vaz@capgemini.com>
2024-01-24 11:00:54 -0300

    WPF Helpers added

---
+++ WPF Helper/Converters/PathToIconConverter.cs
@@ -0,0 +1,19 @@
+﻿using HelperMethods;
+using System.Globalization;
+using System.Windows.Data;
+#pragma warning disable CS8603
+
+namespace WPFHelper.Converters;
+
+public class PathToIconConverter : IValueConverter
+{
+    #region Convert
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value is string path ? SystemMethods.GetIcon(path, true) : null;
+    #endregion
+
+    #region ConvertBack
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+    #endregion
+}
---
+++ WPF Helper/Converters/ReferenceEqualsConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace WPFHelper.Converters;
+
+public class ReferenceEqualsConverter : IMultiValueConverter
+{
+    #region Convert
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) =>
+        values.Length != 0 && values.All(_ => ReferenceEquals(_, values[0]));
+    #endregion
+
+    #region ConvertBack
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+    #endregion
+}
