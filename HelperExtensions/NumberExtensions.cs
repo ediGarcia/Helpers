b902b0c59bbc85009ea47b50f84b6b1585d62f93
avaz <alex.vaz@capgemini.com>
2024-09-17 19:59:48 -0300

    NumberExtensions class added

---
+++ HelperExtensions/NumberExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HelperExtensions;
+
+public static class NumberExtensions
+{
+    #region IsInfinity
+    ///<inheritdoc cref="Double.IsInfinity"/>
+    public static bool IsInfinity(this double value) =>
+        Double.IsInfinity(value);
+    #endregion
+
+    #region IsNaN
+    ///<inheritdoc cref="Double.IsNaN"/>
+    public static bool IsNaN(this double value) =>
+        Double.IsNaN(value);
+    #endregion
+
+    #region IsNegativeInfinity
+    ///<inheritdoc cref="Double.IsNegativeInfinity"/>
+    public static bool IsNegativeInfinity(this double value) =>
+        Double.IsNegativeInfinity(value);
+    #endregion
+
+    #region IsPositiveInfinity
+    ///<inheritdoc cref="Double.IsPositiveInfinity"/>
+    public static bool IsPositiveInfinity(this double value) =>
+        Double.IsPositiveInfinity(value); 
+    #endregion
+}
