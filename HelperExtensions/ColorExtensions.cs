412ba654405382fdba63f776e6f237676306b67a
avaz <alex.vaz@capgemini.com>
2024-09-24 22:37:22 -0300

    GetDarker and GetLighter extension methods created.

--- HelperExtensions/ColorExtensions.cs
+++ HelperExtensions/ColorExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows.Media;
+using HelperMethods;
+
 // ReSharper disable UnusedMember.Global
 
 namespace HelperExtensions;
@@ -22,6 +24,28 @@ public static class ColorExtensions
     #endregion
 
     #region SolidColorBrush
+
+    #region GetDarker
+    /// <summary>
+    /// Returns a new <see cref="SolidColorBrush"/> with a darker shade.
+    /// </summary>
+    /// <param name="brush"></param>
+    /// <param name="percentage"></param>
+    /// <returns></returns>
+    public static SolidColorBrush GetDarker(this SolidColorBrush brush, double percentage = 10) =>
+        ColorMethods.GetDarkerBrush(brush, percentage);
+    #endregion
+
+    #region GetLighter
+    /// <summary>
+    /// Returns a new <see cref="SolidColorBrush"/> with a lighter shade.
+    /// </summary>
+    /// <param name="brush"></param>
+    /// <param name="percentage"></param>
+    /// <returns></returns>
+    public static SolidColorBrush GetLighter(this SolidColorBrush brush, double percentage = 10) =>
+        ColorMethods.GetLighterBrush(brush, percentage);
+    #endregion
 
     #region ToMediaColor
     /// <summary>
