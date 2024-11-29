c1be9f5cfbe85730e284d4365f41b15f69d33a87
avaz <alex.vaz@capgemini.com>
2024-11-29 11:39:54 -0300

    IsStringNullOrWhiteSpaceConverter class added

---
+++ WpfHelpers/Converters/IsStringNullOrWhiteSpaceConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using HelperExtensions;
+
+namespace WpfHelpers.Converters;
+
+public class IsStringNullOrWhiteSpaceConverter : IValueConverter
+{
+    #region Convert
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value?.ToString().IsNullOrWhiteSpace() ?? true;
+    #endregion
+
+    #region ConvertBack
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        throw new InvalidOperationException();
+    #endregion
+}
