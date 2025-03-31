8c28ff3a6583dfeb8939030fe28960498e121c17
avaz <alex.vaz@capgemini.com>
2025-03-31 16:02:36 -0300

    RelayCommand<T> added

--- HelperClasses/Classes/RelayCommand.cs
+++ HelperClasses/Classes/RelayCommand.cs
@@ -1,8 +1,51 @@
 ﻿using System.Windows.Input;
 
+#pragma warning disable CS8600
+#pragma warning disable CS8604
 #pragma warning disable CS8625
 
 namespace HelperClasses.Classes;
+
+public class RelayCommand<T>(Action<T> execute, Func<T, bool> canExecute = null) : ICommand
+{
+    #region Custom Events
+
+    /// <summary>
+    /// Notifies whether the "canExecute" property is changed.
+    /// </summary>
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    #endregion
+
+    private readonly Action<T> _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+
+    #region Public Methods
+
+    #region CanExecute
+    /// <summary>
+    /// Indicates whether the command's action can be executed.
+    /// </summary>
+    /// <param name="parameter"></param>
+    /// <returns></returns>
+    public bool CanExecute(object? parameter) =>
+        canExecute?.Invoke((T)parameter) != false;
+    #endregion
+
+    #region Execute
+    /// <summary>
+    /// Executes the command's action.
+    /// </summary>
+    /// <param name="parameter"></param>
+    public void Execute(object? parameter) =>
+        _execute((T)parameter);
+    #endregion
+
+    #endregion
+}
 
 public class RelayCommand(Action execute, Func<bool> canExecute = null) : ICommand
 {
