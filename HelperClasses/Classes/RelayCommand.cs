7ce18480b0854f5535d303cd47fc8aad35db6752
avaz <alex.vaz@capgemini.com>
2025-04-04 09:46:30 -0300

    RelayCommand classes sorted.

--- HelperClasses/Classes/RelayCommand.cs
+++ HelperClasses/Classes/RelayCommand.cs
@@ -6,47 +6,6 @@
 
 namespace HelperClasses.Classes;
 
-public class RelayCommand<T>(Action<T> execute, Func<T, bool> canExecute = null) : ICommand
-{
-    #region Custom Events
-
-    /// <summary>
-    /// Notifies whether the "canExecute" property is changed.
-    /// </summary>
-    public event EventHandler? CanExecuteChanged
-    {
-        add => CommandManager.RequerySuggested += value;
-        remove => CommandManager.RequerySuggested -= value;
-    }
-
-    #endregion
-
-    private readonly Action<T> _execute = execute ?? throw new ArgumentNullException(nameof(execute));
-
-    #region Public Methods
-
-    #region CanExecute
-    /// <summary>
-    /// Indicates whether the command's action can be executed.
-    /// </summary>
-    /// <param name="parameter"></param>
-    /// <returns></returns>
-    public bool CanExecute(object? parameter) =>
-        canExecute?.Invoke((T)parameter) != false;
-    #endregion
-
-    #region Execute
-    /// <summary>
-    /// Executes the command's action.
-    /// </summary>
-    /// <param name="parameter"></param>
-    public void Execute(object? parameter) =>
-        _execute((T)parameter);
-    #endregion
-
-    #endregion
-}
-
 public class RelayCommand(Action execute, Func<bool> canExecute = null) : ICommand
 {
     #region Custom Events
@@ -83,6 +42,47 @@ public class RelayCommand(Action execute, Func<bool> canExecute = null) : IComma
     /// <param name="parameter"></param>
     public void Execute(object? parameter) =>
         _execute();
+    #endregion
+
+    #endregion
+}
+
+public class RelayCommand<T>(Action<T> execute, Func<T, bool> canExecute = null) : ICommand
+{
+    #region Custom Events
+
+    /// <summary>
+    /// Notifies whether the "canExecute" property is changed.
+    /// </summary>
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    #endregion
+
+    private readonly Action<T> _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+
+    #region Public Methods
+
+    #region CanExecute
+    /// <summary>
+    /// Indicates whether the command's action can be executed.
+    /// </summary>
+    /// <param name="parameter"></param>
+    /// <returns></returns>
+    public bool CanExecute(object? parameter) =>
+        canExecute?.Invoke((T)parameter) != false;
+    #endregion
+
+    #region Execute
+    /// <summary>
+    /// Executes the command's action.
+    /// </summary>
+    /// <param name="parameter"></param>
+    public void Execute(object? parameter) =>
+        _execute((T)parameter);
     #endregion
 
     #endregion
