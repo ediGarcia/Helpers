6e8afb7ac59d238cdc7ec5fd946c653f4a729010
Alex Garcia <eddy.garcia07@gmail.com>
2025-06-26 12:54:04 -0300

    EqualsConverter created

---
+++ WpfHelpers/Converters/EqualsConverter.cs
@@ -0,0 +1,30 @@
+﻿using HelperExtensions;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfHelpers.Converters;
+public class EqualsConverter : IMultiValueConverter
+{
+    #region Convert
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values.IsNullOrEmpty())
+            return false;
+
+        if (values.Length == 1)
+            return true;
+
+        object baseValue = values[0];
+        for (int i = 1; i < values.Length; i++)
+            if (!baseValue.Equals(values[i]))
+                return false;
+
+        return true;
+    }
+    #endregion
+
+    #region ConvertBack
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+    #endregion
+}
