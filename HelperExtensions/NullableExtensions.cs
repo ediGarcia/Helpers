ff7f1827d98c530f2a1fcb54fcf9517fab8c517c
Alex Vaz <alex.vaz@capgemini.com>
2025-09-24 14:48:03 -0300

    Nullable<T>.IsNull extension method added

---
+++ HelperExtensions/NullableExtensions.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable UnusedMember.Global
+
+namespace HelperExtensions;
+
+public static class NullableExtensions
+{
+    #region IsNull
+    /// <summary>
+    /// Gets or sets a value indicating whether the specified nullable object is null.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="obj"></param>
+    /// <returns><see langword="true"/> if <paramref name="obj"/> is null; otherwise, <see langword="false"/>.</returns>
+    public static bool IsNull<T>(this T? obj) where T : struct =>
+        !obj.HasValue;
+    #endregion
+}
