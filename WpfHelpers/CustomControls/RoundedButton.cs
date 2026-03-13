691441821f47741ab1141098f1d646d139a91f34
Alex Vaz <alex.vaz@capgemini.com>
2026-03-13 12:04:02 -0300

    Rounded buttons added.

---
+++ WpfHelpers/CustomControls/RoundedButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfHelpers.CustomControls;
+
+public class RoundedButton : Button
+{
+    #region Property
+
+    /// <summary>
+    /// Gets or sets the corner radius of the button. This property allows you to specify how rounded the corners of the button should be. A higher value will result in more rounded corners, while a value of 0 will produce sharp corners.
+    /// </summary>
+    public CornerRadius CornerRadius
+    {
+        get => (CornerRadius)GetValue(CornerRadiusProperty);
+        set => SetValue(CornerRadiusProperty, value);
+    }
+
+    public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+        nameof(CornerRadius),
+        typeof(CornerRadius),
+        typeof(RoundedButton),
+        new FrameworkPropertyMetadata(
+            new CornerRadius(5),
+            FrameworkPropertyMetadataOptions.AffectsRender
+        )
+    );
+
+    #endregion
+}
---
+++ WpfHelpers/CustomControls/RoundedToggleButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace WpfHelpers.CustomControls;
+
+public class RoundedToggleButton : ToggleButton
+{
+    #region Property
+
+    /// <summary>
+    /// Gets or sets the corner radius of the toggle button. This property allows you to specify how rounded the corners of the button should be. A higher value will result in more rounded corners, while a value of 0 will produce sharp corners.
+    /// </summary>
+    public CornerRadius CornerRadius
+    {
+        get => (CornerRadius)GetValue(CornerRadiusProperty);
+        set => SetValue(CornerRadiusProperty, value);
+    }
+
+    public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+        nameof(CornerRadius),
+        typeof(CornerRadius),
+        typeof(RoundedToggleButton),
+        new FrameworkPropertyMetadata(
+            new CornerRadius(5),
+            FrameworkPropertyMetadataOptions.AffectsRender
+        )
+    );
+
+    #endregion
+}
