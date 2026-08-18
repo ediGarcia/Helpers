524d760c53dfb9af8763b782806084cdaed7179f
avaz <alex.vaz@capgemini.com>
2026-08-18 10:37:41 -0300

    EqualityConverter added

---
+++ WpfHelpers/Converters/EqualityConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfHelpers.Converters;
+
+public class EqualityConverter : IValueConverter
+{
+    #region Convert
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        value?.Equals(parameter) == true;
+    #endregion
+
+    #region ConvertBack
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        value is true ? parameter : Binding.DoNothing;
+    #endregion
+}
